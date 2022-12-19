7eeb443b97b21cf2699eef86159bdc37610c03a4
WarrenRyan <cxtionch@live.com>
2022-12-19 16:17:12 +0800

    add jsonconvert

---
+++ src/TencentQQBot.Sdk/Domain/JsonConverters.cs
@@ -0,0 +1,120 @@
+﻿using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace TencentQQBot.Sdk.Domain;
+/// <summary>
+/// JSON序列化时将 bool 转换为 int<br/>
+/// JSON反序列化时将 int 转换为 bool
+/// </summary>
+public class BoolToInt32Converter : JsonConverter<bool>
+{
+    /// <summary>
+    /// 序列化JSON时 bool 转 int
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value ? 1 : 0);
+    }
+    /// <summary>
+    /// 反序列化JSON时 int 转 bool
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.TokenType switch
+        {
+            JsonTokenType.True => true,
+            JsonTokenType.False => false,
+            JsonTokenType.Number => reader.TryGetInt32(out int num) && Convert.ToBoolean(num),
+            _ => false,
+        };
+    }
+}
+
+/// <summary>
+/// JSON序列化时将 Color 转换为 UInt32<br/>
+/// JSON反序列化时将 UInt32 转换为 Color
+/// </summary>
+public class ColorToUint32Converter : JsonConverter<Color>
+{
+    /// <summary>
+    /// 序列化JSON时 Color 转 int
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue((uint)value.ToArgb());
+    }
+    /// <summary>
+    /// 反序列化JSON时 int 转 Color
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.TokenType switch
+        {
+            JsonTokenType.Number => Color.FromArgb((int)reader.GetUInt32()),
+            _ => Color.Black,
+        };
+    }
+}
+
+/// <summary>
+/// JSON序列化JSON时将 DateTime 转换为 Timestamp<br/>
+/// JSON反序列化JSON时将 Timestamp 转换为 DateTime
+/// </summary>
+public class DateTimeToStringTimestamp : JsonConverter<DateTime>
+{
+    /// <summary>
+    /// 序列化JSON时 DateTime 转 Timestamp
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue($"{value:yyyy-MM-ddTHH:mm:sszzz}");
+    }
+    /// <summary>
+    /// 反序列化JSON时 Timestamp 转 DateTime
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.Number:
+                return DateTime.MinValue.AddMilliseconds(reader.GetDouble());
+            case JsonTokenType.String:
+                string timeStamp = reader.GetString() ?? "0";
+                Match timeMatch = Regex.Match(timeStamp, @"^\d+$");
+                if (timeMatch.Success)
+                {
+                    double offsetVal = double.Parse(timeMatch.Groups[0].Value);
+                    return timeStamp.Length <= 10 ? DateTime.MinValue.AddSeconds(offsetVal) : DateTime.MinValue.AddMilliseconds(offsetVal);
+                }
+                else
+                {
+                    return DateTime.TryParse(timeStamp, out DateTime result) ? result : DateTime.MinValue;
+                }
+            default:
+                return DateTime.MinValue;
+        }
+    }
+}
